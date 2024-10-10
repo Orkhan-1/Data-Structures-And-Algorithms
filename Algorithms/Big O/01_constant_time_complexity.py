02f3a1edcb3628ffd327731b8fd21d93c2df2555
Orkhan <gasanov.orkhan1@gmail.com>
2024-10-10 00:00:50 -0700

    Constant time complexity example

---
+++ Algorithms/Big O/01_constant_time_complexity.py
@@ -0,0 +1,25 @@
+import time
+import matplotlib.pyplot as plt
+
+def access_element(arr, index):
+    return arr[index]
+
+input_sizes = [10, 100, 1000, 10000]
+times = []
+
+for size in input_sizes:
+    arr = list(range(size))
+    index = size // 2
+
+    start_time = time.time()
+    access_element(arr, index)
+    end_time = time.time()
+
+    elapsed_time_ms = (end_time - start_time) * 1000
+    times.append(elapsed_time_ms)
+
+plt.plot(input_sizes, times, marker='o')
+plt.title("O(1) Time Complexity")
+plt.xlabel("Input Size")
+plt.ylabel("Time (milliseconds)")
+plt.show()
